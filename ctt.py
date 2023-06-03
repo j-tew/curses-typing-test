939e8a1e06be9532180cc50a659872b48f74f438
Justin Tew <justinrtew@gmail.com>
2023-06-02 20:43:55 -0500

    Track errors and fix negative cursor position

--- ctt.py
+++ ctt.py
@@ -7,7 +7,6 @@ from curses import wrapper
 #     pass
 
 def get_colors() -> tuple:
-    curses.use_default_colors()
     curses.init_pair(1, curses.COLOR_GREEN, -1)
     curses.init_pair(2, curses.COLOR_RED, -1)
     return curses.color_pair(1), curses.color_pair(2)
@@ -24,6 +23,8 @@ def start_test(ui: curses.window) -> None:
     ui.move(0, 0)
     # Get and check characters until the cursor reaches the end
     GREEN, RED = get_colors()
+    # Track errors
+    errors = 0
     while (cursor_x := ui.getyx()[1]) < len(test_str):
         key = ui.getkey()
         target_char = test_str[cursor_x]
@@ -34,7 +35,7 @@ def start_test(ui: curses.window) -> None:
             # Handle BACKSPACE key
             case 127 | 8:
                 cursor_y, cursor_x = ui.getyx()
-                cursor_x -= 1
+                cursor_x -= 1 if cursor_x > 0 else 0
                 ui.addch(cursor_y, cursor_x, test_str[cursor_x])
                 ui.move(cursor_y, cursor_x)
             # Handle SPACE character
@@ -42,17 +43,25 @@ def start_test(ui: curses.window) -> None:
                 if key == target_char:
                     ui.addstr(key)
                 else:
+                    errors += 1
                     ui.addstr(target_char, RED | curses.A_UNDERLINE)
-            # All other characters (should only accept alphanumeric and punctuation)
             case _:
-                if key == target_char:
-                    ui.addstr(key, GREEN)
-                else:
-                    ui.addstr(key, RED)
+                # Alphanumeric and puncuation characters
+                if curses.ascii.isalnum(key) or curses.ascii.ispunct(key):
+                    if key == target_char:
+                        ui.addstr(key, GREEN)
+                    else:
+                        errors += 1
+                        ui.addstr(key, RED)
         ui.refresh()
+    ui.addstr(1, 0, f'Errors: {errors}')
+    ui.refresh()
+    ui.getkey()
 
 
 def main(ui: curses.window) -> None:
+    # Fix colors
+    curses.use_default_colors()
 
     # Hide cursor
     curses.curs_set(0)
@@ -72,12 +81,13 @@ if __name__ == '__main__':
 #   - [ ] Center the text
 #   - [X] Write characters on the same line
 #   - [X] Make inputs overwite with color
-#   - [ ] Handle only needed keys
+#   - [X] Handle only needed keys
 #   - [X] Track cursor position
-#   - [ ] Handle space character (should print if correct character, but underline in red if incorrect)
+#   - [X] Handle space character (should print if correct character, but underline in red if incorrect)
 #   - [ ] Find text samples
-#   - [ ] Track errors
-#   - [ ] Track time
+#   - [X] Track errors
+#   - [ ] Track time and calulate WPM
 #   - [ ] Accept args for time duration
 #   - [ ] Improve menu
+#   - [X] Fix -x cursor
 

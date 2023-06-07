0f7bfdc6fa0fb1658d168b29f25e181d514946ef
Justin Tew <justinrtew@gmail.com>
2023-06-07 00:28:03 -0500

    Try to fix test rendering; break it even more

--- ctt.py
+++ ctt.py
@@ -3,6 +3,9 @@ import curses
 import curses.ascii
 from curses import wrapper
 from random import choices
+from typing import Any
+
+Window = Any
 
 # def timed_test(duration: int = 30) -> None:
 #     pass
@@ -12,28 +15,28 @@ def get_colors() -> tuple:
     curses.init_pair(2, curses.COLOR_RED, -1)
     return curses.color_pair(1), curses.color_pair(2)
 
-def center_text(window, text: str) -> tuple:
-    begin_y, begin_x = window.getbegyx()
+def centered_window(win_height: int = 13, win_width: int = 80) -> Window:
+    begin_y = (curses.LINES - 1) // 2 - win_height // 2
+    begin_x = (curses.COLS - 1) // 2 - win_width // 2
+    window = curses.newwin(win_height, win_width, begin_y, begin_x)
+    window.box()
+    return window
+
+def centered_text(window: Window, text: str) -> tuple:
     win_height, win_width = window.getmaxyx()
     cursor_y = win_height // 2
     cursor_x = win_width // 2 - len(text) // 2
+    window.addstr(cursor_y, cursor_x, text)
+    window.refresh()
     return cursor_y, cursor_x
 
-def center_win(win_height: int = 13, win_width: int = 80) -> tuple:
-    begin_y = (curses.LINES - 1) // 2 - win_height // 2
-    begin_x = (curses.COLS - 1) // 2 - win_width // 2
-    return win_height, win_width, begin_y, begin_x
-
-def show_menu(stdscr) -> None:
-    win = curses.newwin(*center_win())
+def show_menu() -> None:
+    win = centered_window()
     win.box()
-    stdscr.refresh()
     menu_text = 'Press any key to start...'
-    text_y, text_x = center_text(win, menu_text)
+    text_y, text_x = centered_text(win, menu_text)
     win.addstr(text_y, text_x, menu_text)
-    win.refresh()
-    stdscr.getch()
-    stdscr.clear()
+    win.getch()
 
 def get_text_sample(length: int = 10) -> str:
     with open('words.txt', 'r') as words:
@@ -42,47 +45,43 @@ def get_text_sample(length: int = 10) -> str:
     return ' '.join(sample_words)
 
 def start_test(stdscr) -> None:
-    win = curses.newwin(*center_win())
-    win.box()
-    text_sample = get_text_sample()
-    cursor_y, cursor_x = center_text(win, text_sample)
-    home = cursor_x
-    end = home + len(text_sample)
-    str_loc = [x for x in range(home, end)]
-    win.addstr(cursor_y, cursor_x, text_sample)
-    stdscr.move(cursor_y, home)
-    GREEN, RED = get_colors()
     global errors
     errors = 0
-    while (cursor_x := stdscr.getyx()[1]) in str_loc:
-        key = stdscr.getkey()
-        target_char = text_sample[cursor_x - home]
+    window = centered_window()
+    stdscr.refresh()
+    GREEN, RED = get_colors()
+    text_sample = get_text_sample()
+    cursor_y, cursor_x = centered_text(window, text_sample)
+    window.move(cursor_y, cursor_x)
+    for i, char in enumerate(text_sample):
+        cursor = window.getyx()
+        key = window.getkey()
         match ord(key):
-            # ESC
-            case 27:
-                break
+        # Throws error if the key falls out of ASCII range
             # BACKSPACE
             case 127 | 8:
-                cursor_y, cursor_x = stdscr.getyx()
-                cursor_x -= 1 if cursor_x > 0 else 0
-                win.addch(cursor_y, cursor_x, text_sample[cursor_x - home])
-                win.move(cursor_y, cursor_x)
+                try:
+                    prev_char = text_sample[i - 1]
+                    window.move(cursor_y, cursor[1] - 1)
+                    window.addch(prev_char)
+                except IndexError:
+                    window.move(cursor_y, cursor_x)
             # SPACE
             case 32:
-                if key == target_char:
-                    stdscr.addstr(key)
+                if char == ' ':
+                    window.move(cursor_y, cursor_x + 1)
                 else:
                     errors += 1
-                    stdscr.addstr(target_char, RED | curses.A_UNDERLINE)
+                    window.addstr(char, RED | curses.A_UNDERLINE)
             case _:
                 # Alphanumeric and puncuation
                 if curses.ascii.isalnum(key) or curses.ascii.ispunct(key):
-                    if key == target_char:
-                        stdscr.addstr(key, GREEN)
+                    if key == char:
+                        window.addstr(key, GREEN)
                     else:
                         errors += 1
-                        stdscr.addstr(key, RED)
-        win.refresh()
+                        window.addstr(key, RED)
+        window.refresh()
     stdscr.clear()
 
 def show_results(stdscr):
@@ -95,7 +94,7 @@ def main(stdscr) -> None:
     curses.use_default_colors()
     # curses.curs_set(0)
     stdscr.clear()
-    show_menu(stdscr)
+    show_menu()
     start_test(stdscr)
     show_results(stdscr)
 
